86b79983bbb6b1d6786d115e788492099eff37e2
Pirevan <pirevan@gmail.com>
2018-07-20 15:21:58 +1000

    random testing

--- spec/integration/creates_a_new_user_spec.rb
+++ spec/integration/creates_a_new_user_spec.rb
@@ -10,8 +10,22 @@ feature "creates a new user" do
 		click_button "Create User"
 
 	end
+end
 
-feature "creates a new task"
+feature "logs in" do 
+	scenario "successfully creates a new user and logs in" do 
+		visit root_path
+		click_link "sign up"
+		fill_in "user_email", with: "sam@sam.com"
+		fill_in "user_password", with: "sam123"
+		click_button "Create User"
+		visit root_path
+		expect(page).to have_link("sign out")
+
+	end	
+end
+
+feature "creates a new task" do
 	scenario "successfully creates a new task" do 
 		visit root_path
 		click_link "sign up"
